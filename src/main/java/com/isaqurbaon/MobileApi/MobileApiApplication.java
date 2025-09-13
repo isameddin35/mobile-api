8b3ef2e1d52a182de2a797d7cc0f76a6eee27661
Isameddin <isaqurbanov112@gmail.com>
2025-09-13 21:07:16 +0400

    first commit

---
+++ src/main/java/com/isaqurbaon/MobileApi/MobileApiApplication.java
@@ -0,0 +1,13 @@
+package com.isaqurbaon.MobileApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileApiApplication.class, args);
+	}
+
+}
