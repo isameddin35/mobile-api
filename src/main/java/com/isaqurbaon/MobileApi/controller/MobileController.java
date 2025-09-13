a6057e595215dadf3006a07fd8dd61152184d62f
Isameddin <isaqurbanov112@gmail.com>
2025-09-13 21:41:47 +0400

    Student Entity added

--- src/main/java/com/isaqurbaon/MobileApi/controller/MobileController.java
+++ src/main/java/com/isaqurbaon/MobileApi/controller/MobileController.java
@@ -1,13 +1,15 @@
 package com.isaqurbaon.MobileApi.controller;
 
+import com.isaqurbaon.MobileApi.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MobileController {
+    Student student1 = new Student(1L, "Isa", 20);
 
     @GetMapping("greet")
-    public String greet() {
-        return "Hello Mobile World";
+    public Student greet() {
+        return student1;
     }
 }
